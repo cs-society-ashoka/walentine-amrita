aef0a78295ede708da1171a4ab0e9dc346b35d02
cs-society-ashoka <cs.society@ashoka.edu.in>
2026-02-12 23:44:19 +0530

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/walentine-website-kiah/",
+  base: "/walentine-website-granthana/",
   plugins: [react()],
   test: {
     environment: "jsdom",
